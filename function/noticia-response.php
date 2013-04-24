c977f4e71113a1d3e33a9db323f17d5156658136
Ignacio Poblete <nacho1593@hotmail.com>
2013-04-23 20:15:55 -0400

    ingresar noticia (admin)

--- function/noticia-response.php
+++ function/noticia-response.php
@@ -6,7 +6,7 @@
             $noticia = new noticiaDAL();
             $resultado=$noticia->ingresarNoticia($_POST['titulo'],'noticia','imagen');
             return $resultado;
-            $arr = array("re"=>$resultado);
-            return json_encode($arr);
+//            $arr = array("re"=>$resultado);
+//            return json_encode($arr);
         }
 ?>
