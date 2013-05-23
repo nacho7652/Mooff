0643938995de274d85821f08674e73e5c5d20546
ignacio <nacho1593@hotmail.com>
2013-05-22 23:36:47 -0400

    login face arreglado

--- index.php
+++ index.php
@@ -8,12 +8,13 @@
     require_once 'DAL/institucionDAL.php';
     date_default_timezone_set("Chile/Continental");
     //face
-      include_once ('function/facebook-response.php');   
+        
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
+        
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -26,30 +27,9 @@
         <link rel="stylesheet" href="css/style-own.css">
         <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
         <meta charset="utf-8">
+       
     </head>
     <body>
-       <div id="top-login">
-           <div class="inner-toplogin">
-               <div class="inner-toploginleft">
-                   <div class="itemtoplog">
-                       <div class="input-item">
-                           <div class="field-item">Email</div>
-                           <input id="mail" class="input-style" type="text" />
-                       </div>
-                       <div class="input-item">
-                            <div class="field-item">Password</div>
-                            <input id="pass" class="input-style" type="password" />
-                       </div>
-                   </div>
-                   <div class="itemtoplog">
-                       <a class="loginface-top" id="login-fb" href="<?php echo $loginUrl; ?>">
-                            <div id="loginbtn-fb"></div>
-                            <div class="txtfb">Ingresar con Facebook</div>
-                        </a>
-                   </div>
-               </div>
-           </div>
-       </div>
        
         <div id="coverall">
             <div class="innercal">
@@ -64,12 +44,12 @@
                 </div>
             </div>
         </div>
-        
         <div id="top">
             <div class="inner-top">
                 <div id="logmooff"></div>
                 <div class="part-right-top">
                     <?php 
+                    include_once ('function/facebook-response.php');
                     if(isset($user_profile) != null){//apreté el boton y se creo mi usuario
                         $_SESSION['user'] = $user_profile;
                         $usuario = new usuarioDAL();
@@ -77,7 +57,10 @@
                     }
                     
                     if(!isset($_SESSION['user'])){?>
-                        in
+                        <a id="login-fb" href="<?php echo $loginUrl; ?>">
+                            <div id="loginbtn-fb"></div>
+                            <div class="txtfb">Ingresar con Facebook</div>
+                        </a>
                     <?php }else{?>
                         <div id="partuser">
                             <div class="flecha-op"></div>
@@ -133,50 +116,3 @@
                     </div>
                     <div class="redes-bott">
                        
-                    </div>
-                </div>
-                
-            </div>
-            <!-- SCM Music Player http://scmplayer.net -->
-            <script type="text/javascript" src="http://scmplayer.net/script.js" 
-            data-config="{'skin':'skins/black/skin.css','volume':50,'autoplay':true,'shuffle':false,'repeat':1,'placement':'bottom','showplaylist':false,'playlist':'http://soundcloud.com/nubeswing/sets/varios'}" ></script>
-            <!-- SCM Music Player script end -->
-
-        </footer>
-         <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-         <script type="text/javascript" src="js/script.js"></script>
-         <script type="text/javascript" src="js/jquery.cycle.all.latest.js"></script>
-<!--          <script type="text/javascript">
-            $(document).ready(function() {
-                /*SLIDE-BANNER*/
-                $('.slideshow-first').fadeTo(5000, 0, function(){
-                   
-                });
-                $('.slideshow').show(); 
-                   $('.slideshow').cycle({
-                                   fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-                           });
-                   });
-        /******/
-        
-                
-        </script>-->
-        <script type="text/javascript" src="highslide/highslide-with-gallery.js"></script>
-        <script type="text/javascript" src="highslide/highslide.config.js" charset="utf-8"></script>
-        <link rel="stylesheet" type="text/css" href="highslide/highslide.css" />
-         <script type="text/javascript" src="js/scroll.js" charset="utf-8"></script>
-         <script id="_wau0ng">var _wau = _wau || [];
-            _wau.push(["tab", "nkj1k09uvt9w", "0ng", "left-upper"]);
-            (function() {var s=document.createElement("script"); s.async=true;
-            s.src="http://widgets.amung.us/tab.js";
-            document.getElementsByTagName("head")[0].appendChild(s);
-            })();
-         </script>
-
-        <!--[if lt IE 7]>
-        <link rel="stylesheet" type="text/css" href="highslide/highslide-ie6.css" />
-        <![endif]-->
-
-        
-    </body>
-</html>
